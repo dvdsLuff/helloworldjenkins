ed9edc80363f2a38cd26d3668aae2916f2f5512d
dvdsLuff <thomasluff@hotmail.com>
2021-03-05 14:37:11 +0100

    added comment to check hook

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
+//hej
+
 public class HelloworldApplication {
 
     public static void main(String[] args) {
